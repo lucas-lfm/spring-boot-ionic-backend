4c6a68239a11e7f373737b21f41e2748cd28e2e7
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-03-24 13:10:06 -0300

    Produto e associação muitos para muitos com Categoria

--- src/main/java/com/springudemy/cursomc/CursomcApplication.java
+++ src/main/java/com/springudemy/cursomc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.springudemy.cursomc.domain.Categoria;
+import com.springudemy.cursomc.domain.Produto;
 import com.springudemy.cursomc.repositories.CategoriaRepository;
+import com.springudemy.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository catRepo;
+	
+	@Autowired
+	private ProdutoRepository proRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +32,26 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat2 = new Categoria(null, "Escritório");
 		Categoria cat3 = new Categoria(null, "Limpeza");
 		
+		Produto p1 = new Produto(null, "Computador", 3000.00);
+		Produto p2 = new Produto(null, "Mouse", 80.00);
+		Produto p3 = new Produto(null, "Impressora", 1000.00);
+		Produto p4 = new Produto(null, "Grampeador 26/6", 35.00);
+		Produto p5 = new Produto(null, "Carimbo Grande", 60.00);
+		Produto p6 = new Produto(null, "Álcool 70%", 15.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p3, p4, p5));
+		cat3.getProdutos().addAll(Arrays.asList(p6));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p4.getCategorias().addAll(Arrays.asList(cat2));
+		p5.getCategorias().addAll(Arrays.asList(cat2));
+		p6.getCategorias().addAll(Arrays.asList(cat3));
+		
 		catRepo.saveAll(Arrays.asList(cat1, cat2, cat3));
+		proRepo.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6));
 		
 	}
 
