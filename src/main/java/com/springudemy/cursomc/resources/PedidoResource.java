6f06019734c817f5a857cfcd221d1efd9a24514f
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-03-26 16:09:12 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/springudemy/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.springudemy.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springudemy.cursomc.domain.Pedido;
+import com.springudemy.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")
+public class PedidoResource {
+	
+	@Autowired
+	private PedidoService catServ;
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Pedido obj = catServ.find(id);
+		
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/springudemy/cursomc/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.springudemy.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springudemy.cursomc.domain.Pedido;
+import com.springudemy.cursomc.repositories.PedidoRepository;
+import com.springudemy.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository catRepo;
+	
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = catRepo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
+				", Tipo: " + Pedido.class.getName()));
+	}
+	
+}
