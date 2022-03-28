d230dc6cc404f7b6599055e3923ccb5874a86ea1
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-03-28 15:52:14 -0300

    Validação sintática com Bean validation

--- src/main/java/com/springudemy/cursomc/services/CategoriaService.java
+++ src/main/java/com/springudemy/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.springudemy.cursomc.domain.Categoria;
+import com.springudemy.cursomc.dto.CategoriaDTO;
 import com.springudemy.cursomc.repositories.CategoriaRepository;
 import com.springudemy.cursomc.services.exceptions.DataIntegrityException;
 import com.springudemy.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return catRepo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO obj) {
+		return new Categoria(obj.getId(), obj.getNome());
 	}
 	
 }
