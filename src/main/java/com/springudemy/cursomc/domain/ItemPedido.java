bedabdf34a3db8314a1949048451c115fcbb827e
Lucas Mendes <mendes.lucas.lfm@gmail.com>
2022-03-30 11:14:19 -0300

    Inserindo Pedido

--- src/main/java/com/springudemy/cursomc/domain/ItemPedido.java
+++ src/main/java/com/springudemy/cursomc/domain/ItemPedido.java
@@ -39,9 +39,17 @@ public class ItemPedido implements Serializable{
 		return id.getProduto();
 	}
 	
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+	
 	@JsonIgnore
 	public Pedido getPedido() {
 		return id.getPedido();
+	}
+	
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
 	}
 	
 	public ItemPedidoPK getId() {
--- src/main/java/com/springudemy/cursomc/services/PedidoService.java
+++ src/main/java/com/springudemy/cursomc/services/PedidoService.java
@@ -1,11 +1,18 @@
 package com.springudemy.cursomc.services;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.springudemy.cursomc.domain.ItemPedido;
+import com.springudemy.cursomc.domain.PagamentoComBoleto;
 import com.springudemy.cursomc.domain.Pedido;
+import com.springudemy.cursomc.domain.enums.EstadoPagamento;
+import com.springudemy.cursomc.repositories.ItemPedidoRepository;
+import com.springudemy.cursomc.repositories.PagamentoRepository;
 import com.springudemy.cursomc.repositories.PedidoRepository;
 import com.springudemy.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -13,12 +20,49 @@ import com.springudemy.cursomc.services.exceptions.ObjectNotFoundException;
 public class PedidoService {
 
 	@Autowired
+	private BoletoService bolServ;
+	
+	@Autowired 
+	private ProdutoService proServ;
+	
+	@Autowired
 	private PedidoRepository pedRepo;
+	
+	@Autowired
+	private PagamentoRepository pagRepo;
+	
+	@Autowired ItemPedidoRepository ipRepo;
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = pedRepo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + 
 				", Tipo: " + Pedido.class.getName()));
+	}
+	
+	@Transactional
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		obj.setInstante(new Date());
+		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
+		obj.getPagamento().setPedido(obj);
+		
+		if (obj.getPagamento() instanceof PagamentoComBoleto) {
+			PagamentoComBoleto pagto = (PagamentoComBoleto) obj.getPagamento();
+			bolServ.preencherPagamentoComBoleto(pagto, obj.getInstante());
+		}
+		
+		obj = pedRepo.save(obj);
+		pagRepo.save(obj.getPagamento());
+		
+		for(ItemPedido ip: obj.getItens()) {
+			ip.setDesconto(0.0);
+			ip.setPreco(proServ.find(ip.getProduto().getId()).getPreco());
+			ip.setPedido(obj);
+		}
+		
+		ipRepo.saveAll(obj.getItens());
+		
+		return obj;
 	}
 	
 }
